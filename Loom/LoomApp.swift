dd4c75a3a2ed927f6ddbc7158da8ce9e96323de0
LorenzoPizzuto <lorenzopizzuto05@gmail.com>
2024-12-16 16:15:12 +0100

    Initial Commit

---
+++ Loom/LoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoomApp.swift
+//  Loom
+//
+//  Created by Lorenzo Pizzuto on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
